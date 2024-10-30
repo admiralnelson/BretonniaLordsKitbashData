eeba47a14a02d3d5492d25b62d735b2408990a7a
admiralnelson <admiralofinternetmeme@outlook.com>
2024-10-30 12:29:54 +0700

    assumed to be working with "agent units" like champion, but due to engine limitation remove these guys on the next commits

--- campaign/mod/SwitchLordsAppearance.ts
+++ campaign/mod/SwitchLordsAppearance.ts
@@ -19,6 +19,22 @@ namespace BretonniaLordsKitbashData {
     )
 
     x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
+        "admiralnelson_bret_paladin_massif_agent_key",
+        (_, characther) => {
+            x.console.warn("charnging admiralnelson_bret_paladin_massif_agent_key apperarance!")
+            characther.ChangeModelAppearance("admiralnelson_brt_generic_paladin_massif_2handed_art_key_override")
+        }
+    )
+
+    x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
+        "admiralnelson_bret_paladin_massif_sword_shield_agent_key",
+        (_, characther) => {
+            x.console.warn("charnging admiralnelson_bret_paladin_massif_sword_shield_agent_key apperarance!")
+            characther.ChangeModelAppearance("admiralnelson_brt_generic_paladin_massif_sword_shield_art_key_override")
+        }
+    )
+
+    x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
         "admiralnelson_bret_lord_2handed_agent_key",
         (agentKey, characther) => {
             x.console.warn("Changing admiralnelson_bret_lord_2handed_agent_key appearance!")
@@ -31,6 +47,22 @@ namespace BretonniaLordsKitbashData {
         (agentKey, characther) => {
             x.console.warn("Changing wh_main_brt_lord appearance!")
             characther.ChangeModelAppearance("wh_main_art_set_brt_lord_01")
+        }
+    )
+
+    x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
+        "admiralnelson_bret_paladin_2handed_agent_key",
+        (agentKey, characther) => {
+            x.console.warn("changing admiralnelson_bret_paladin_2handed_agent_key")
+            characther.ChangeModelAppearance("admiralnelson_brt_generic_paladin_2handed_art_key_override")
+        }
+    )
+
+    x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
+        "admiralnelson_bret_paladin_2handed_agent_key",
+        (agentKey, characther) => {
+            x.console.warn("changing admiralnelson_bret_paladin_2handed_agent_key")
+            characther.ChangeModelAppearance("admiralnelson_brt_generic_paladin_2handed_art_key_override")
         }
     )
 
