3dfd43a2e1b2f08dcce11bef7a40b554e043da43
admiralnelson <admiralofinternetmeme@outlook.com>
2024-11-03 11:39:26 +0700

    converted from 1h sword shield massif paladin to another 1h sword shield massif lord variant

--- campaign/mod/SwitchLordsAppearance.ts
+++ campaign/mod/SwitchLordsAppearance.ts
@@ -27,10 +27,10 @@ namespace BretonniaLordsKitbashData {
     )
 
     x.KitbashedCharacter.RegisterCallbackBeforeEnablingKitbash(
-        "admiralnelson_bret_paladin_massif_sword_shield_agent_key",
+        "admiralnelson_bret_lord_massif_sword_shield_agent_key_1",
         (_, characther) => {
-            x.console.warn("charnging admiralnelson_bret_paladin_massif_sword_shield_agent_key apperarance!")
-            characther.ChangeModelAppearance("admiralnelson_brt_generic_paladin_massif_sword_shield_art_key_override")
+            x.console.warn("charnging admiralnelson_bret_lord_massif_sword_shield_agent_key_1 apperarance!")
+            characther.ChangeModelAppearance("admiralnelson_brt_generic_lord_massif_sword_shield_art_key_override_1")
         }
     )
 
